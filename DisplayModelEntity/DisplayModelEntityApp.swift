a9e03e371e4220f537f9a3397592a01fa3391d4e
Stefano Rebulla <stefano.rebulla@gmail.com>
2024-10-21 05:02:47 +0200

    Initial Commit

---
+++ DisplayModelEntity/DisplayModelEntityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayModelEntityApp.swift
+//  DisplayModelEntity
+//
+//  Created by Stefano Rebulla on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DisplayModelEntityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
